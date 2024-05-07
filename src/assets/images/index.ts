22ec34cd2e65584e011a74d0ce7d1fddb1fdecde
sasabilas <salsabilasuryawan@gmail.com>
2024-05-07 17:05:17 +0800

    add assets for achievement

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,34 @@
 import logo from "./psm.png";
-
 import structure from "./structure.png";
 import news from "./news.png";
+import achiev from "./achiev.png";
+import achievement1 from "./achievement/2006.png";
 
-export { logo, structure, news };
+import fIndonesia from "./achievement/indonesia.svg";
+import fChina from "./achievement/china.svg";
+import fAustria from "./achievement/austria.svg";
+import fItaly from "./achievement/italy.svg";
+import fJapan from "./achievement/japan.svg";
+import fPoland from "./achievement/poland.svg";
+import fPortugal from "./achievement/southafrica.svg";
+import fSouthKorean from "./achievement/southkorean.svg";
+import fSouthAfrica from "./achievement/southafrica.svg";
+import fUnitedStates from "./achievement/unitedstates.svg";
+
+export {
+  logo,
+  structure,
+  news,
+  achiev,
+  achievement1,
+  fIndonesia,
+  fChina,
+  fAustria,
+  fItaly,
+  fJapan,
+  fPoland,
+  fPortugal,
+  fSouthAfrica,
+  fSouthKorean,
+  fUnitedStates
+};
