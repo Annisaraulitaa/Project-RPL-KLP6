cb2ec07c186a97a1253b352ef5b8788ef2acf0bd
Annisaraulitaa <annisaraulita240702@gmail.com>
2024-05-29 02:08:12 +0800

    feat: add images for news articles and committee members

--- src/assets/mockImages/index.ts
+++ src/assets/mockImages/index.ts
@@ -1,3 +1,22 @@
 import user from "./user.png";
+import news1 from "./news1.jpg";
+import news2 from "./news2.jpg";
 
-export { user };
+import ketua from "./ketua.jpg";
+import wakil from "./wakil.jpg";
+import sekretaris from "./sekretaris.jpg";
+import bendahara from "./bendahara.jpg";
+import conductor from "./conductor.jpg";
+import arranger from "./arranger.jpg";
+
+export {
+  user,
+  news1,
+  news2,
+  ketua,
+  wakil,
+  sekretaris,
+  bendahara,
+  conductor,
+  arranger,
+};
